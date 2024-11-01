f5372e7a327e657e0c3b8c6fa040f92983f18c4c
Liam Mah <liam.d.mah@gmail.com>
2024-10-31 18:21:28 -0600

    Revising some test scripts to be functional with new directory structure.

--- TestScripts/Plane/sitlTakeoff.py
+++ TestScripts/Plane/sitlTakeoff.py
@@ -8,6 +8,3 @@ import Operations.Copter.takeoff as takeoff
 import Operations.General.waypoint as waypoint
 
 vehicle_connection, valid_connection= initialize.connect_to_vehicle('udpin:127.0.0.1:14550')
-
-mode.set_mode(vehicle_connection, 9)
-
