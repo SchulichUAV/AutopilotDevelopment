eb281e15331dfc63a43899a4597d49890dc52f05
Liam Mah <liam.d.mah@gmail.com>
2024-11-03 12:53:51 -0700

    testing alternative takeoff script.

--- TestScripts/Plane/sitlWaypoint.py
+++ TestScripts/Plane/sitlWaypoint.py
@@ -10,13 +10,12 @@ import Operations.General.arm as arm
 import Operations.General.initialize as initialize
 import Operations.General.mode as mode
 import Operations.Copter.takeoff as takeoff
-import Operations.General.waypoint as waypoint
+import Operations.Copter.waypoint as waypoint
 
 vehicle_connection, valid_connection= initialize.connect_to_vehicle('udpin:127.0.0.1:14550')
 
 arm.arm(vehicle_connection)
 time.sleep(1)
-# mode.set_mode(vehicle_connection, 13)
-takeoff.takeoff(vehicle_connection, 20)
-time.sleep(1)
-
+mode.set_mode(vehicle_connection, 13)
+time.sleep(15)
+waypoint.absolute_movement(vehicle_connection, 0, 0, 100)
