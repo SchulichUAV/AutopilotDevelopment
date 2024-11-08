5ff19a77e43cc4fe5a8ad62005e37647dc7aa8da
Bryce <bryceilcan29@gmail.com>
2024-11-07 19:34:57 -0700

    adding speedTest script

---
+++ Plane/TestScripts/sitlChangeSpeed.py
@@ -0,0 +1,28 @@
+from pymavlink import mavutil
+import time
+import sys
+import os
+
+script_dir = os.path.abspath('./../..')
+sys.path.append(script_dir)
+
+import General.Operations.arm as arm
+import General.Operations.initialize as initialize
+import General.Operations.mode as mode
+import General.Operations.speed as speed
+
+import Plane.Operations.takeoffConfiguration as takeoff_configuration
+
+vehicle_connection, valid_connection= initialize.connect_to_vehicle('udpin:127.0.0.1:14550')
+
+takeoff_configuration.set_takeoff_altitude(vehicle_connection, 100)
+takeoff_configuration.set_takeoff_angle(vehicle_connection, 20)
+time.sleep(1)
+arm.arm(vehicle_connection)
+time.sleep(1)
+mode.set_mode(vehicle_connection, 13)
+
+time.sleep(20)
+
+#vehicle_connection, speed, throttle=-1
+speed.speed(vehicle_connection, 100)
