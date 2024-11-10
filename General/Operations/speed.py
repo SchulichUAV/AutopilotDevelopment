dad9c7b0974a34b12b811d3340b0708f12976bc3
Bryce <bryceilcan29@gmail.com>
2024-11-09 18:27:18 -0700

    fixed quotations

--- General/Operations/speed.py
+++ General/Operations/speed.py
@@ -27,33 +27,9 @@ def change_speed(vehicle_connection, speed, throttle=-1):
     msg = vehicle_connection.recv_match(type='COMMAND_ACK', blocking=True) # Print ACK to confirm successful execution
     print(msg)
 
-# def set_airspeed_cruise(vehicle_connection, speed, throttle=-1):
-#     # PROMISES: Sets fixed-wing vehicle takeoff altitude configuration
-#     # REQUIRES: Vehicle connection, the desired height to take off to
-#     vehicle_connection.mav.param_set_send(
-#         vehicle_connection.target_system,
-#         vehicle_connection.target_component,
-#         mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, # Specify speed command
-#         0, # Confirmation - 0: First transmission of this cmd, 1-255: Confirmation transmissions (e.g. kill)
-#         mavutil.mavlink.SPEED_TYPE_AIRSPEED, # Param 1 - setting speed type
-#         speed, # Param 2 - speed in m/s
-#         throttle, # Param 3 - throttle in percent
-#         0, # Param 4 - Unused, set to zero to populate all 7 parameters
-#         0, # Param 5 - Unused, set to zero to populate all 7 parameters
-#         0, # Param 6 - Unused, set to zero to populate all 7 parameters
-#         0 # Param 7 - Unused, set to zero to populate all 7 parameters
-#     )
-#     print(vehicle_connection.recv_match(type='COMMAND_ACK', blocking=True))
-
-# Connects to the vehicle and allows the operator to change fixed-wing takeoff configurations
-
-# Created by: Liam Mah, November 2024
-
-from pymavlink import mavutil
-
 def set_cruise_speed(vehicle_connection, speed):
-    # PROMISES: Sets fixed-wing vehicle takeoff altitude configuration
-    # REQUIRES: Vehicle connection, the desired height to take off to
+    # PROMISES: Sets fixed-wing vehicle speed in m/s
+    # REQUIRES: Vehicle connection, the desired vehicle speed
     vehicle_connection.mav.param_set_send(
         vehicle_connection.target_system,
         vehicle_connection.target_component,
