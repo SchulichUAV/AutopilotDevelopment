bb18217ce25d0e9d5a56c1b50677282e43611df9
Oscar Spencer <oscarspencerdev@gmail.com>
2024-11-22 21:42:49 +0000

    new reset

--- Plane/TestScripts/sitlTakeoff.py
+++ Plane/TestScripts/sitlTakeoff.py
@@ -9,6 +9,7 @@ sys.path.append(script_dir)
 import General.Operations.arm as arm
 import General.Operations.initialize as initialize
 import General.Operations.mode as mode
+import General.Operations.altitude as altitude
 
 import Plane.Operations.takeoffConfiguration as takeoff_configuration
 
@@ -21,55 +22,4 @@ arm.arm(vehicle_connection)
 time.sleep(1)
 mode.set_mode(vehicle_connection, 13)
 
-def set_altitude(altitude, frame=mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT):
-
-    vehicle_connection.wait_heartbeat()
-
-    target_system = vehicle_connection.target_system
-    target_component = vehicle_connection.target_component
-
-    vehicle_connection.mav.set_position_target_global_int_send(
-        0, # time since booting
-        target_system,
-        target_component,
-        frame, 
-        0b0000111111000111, # set altitude mask
-        0,
-        0,
-        altitude,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-    )
-
-    print(f"Altitude now set to: {altitude} meters.")
-
-def get_altitude():
-    vehicle_connection.wait_heartbeat()
-
-    vehicle_connection.mav.request_data_stream_send(
-
-        vehicle_connection.target_system,
-        vehicle_connection.target_component,
-        mavutil.mavlink.MAV_DATA_STREAM_ALL,
-        1,
-        1
-    )
-
-    msg = vehicle_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
-    altitude_meters = msg.alt 
-
-    print(f"Current altitude: {altitude_meters}") 
-
-
-
-set_altitude(100)
-
-time.sleep(30) # just for testing if the altitude changes
-
-get_altitude()
+altitude.set_altitude(vehicle_connection, 300)
