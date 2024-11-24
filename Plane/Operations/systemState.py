634a2d265ad2a6e9b08ff8130180342fc36eda93
user <steven03721@gmail.com>
2024-11-24 20:17:36 +0000

    functions for general vehicle system state

---
+++ Plane/Operations/systemState.py
@@ -0,0 +1,51 @@
+# Connects to the vehicle and retrieves the general system vehicle system state
+
+from pymavlink import mavutil
+
+def receive_system_status(vehicle_connection):
+    # PROMISES: Retreives general system state information of plane
+    # REQUIRES: Vehicle connection
+    return vehicle_connection.recv_match(type='SYS_STATUS', blocking='True')
+
+def receive_param_request_read(vehicle_connection, param_id):
+    param_id_bytes = param_id.encode('utf-8')
+    # PROMISES: Retreives value of parameter thats passed in
+    # REQUIRES: Vehicle connection, the desired parameter thats passed in
+    vehicle_connection.mav.param_request_read_send(
+        vehicle_connection.target_system,
+        vehicle_connection.target_component,
+        param_id_bytes,
+        -1,
+    )
+
+    while True:
+        message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True, timeout=5)
+        if message is None:
+            print("Timeout: No response recieved.")
+            return None
+
+        param_id_value = message.param_id
+
+        if isinstance(param_id_value, bytes):
+            param_id_value = param_id_value.decode('utf-8').strip('\x00')
+        else:
+            param_id_value= param_id_value.strip('\x00')
+
+        if param_id_value == param_id:
+            return message.param_value
+
+def receive_gps_raw(vehicle_connection):
+    # PROMISES: Retrieves general gps data
+    # REQUIRES: Vehicle connection
+    return vehicle_connection.recv_match(type='GPS_RAW_INT', blocking='True')
+
+# def receive_gps_status(vehicle_connection):
+#     # PROMISES:
+#     # REQUIRES:
+#     return vehicle_connection.recv_match(type='GPS_STATUS', blocking='True')
+
+# def receive_scaled_imu(vehicle_connection):
+#     # PROMISES:
+#     # REQUIRES:
+#     vehicle_connection.recv_match(type='SCALED_IMU', blocking='True')
+
