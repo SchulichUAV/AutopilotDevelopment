1487acb190b26bef9377f9b1494b2ff8d291d423
Nathan <nathanjourdain26@gmail.com>
2024-12-01 01:59:58 +0000

    Added functionality to only allow air speed to be set between certain values

--- General/Operations/speed.py
+++ General/Operations/speed.py
@@ -1,14 +1,52 @@
 # Provides functionality to change the speed
 
-# Created by: Bryce Ilcan, november 7 2024
+# Created by: Bryce Ilcan, November 7th, 2024
+# Valid Speed Check added by: Nathan Jourdain, November 30th, 2024
 
 #TODO: Conduct safety checks with physical vehicle [IMPORTANT!]
 
 from pymavlink import mavutil
+import General.Operations.initialize as initialize
+
+vehicle_connection, valid_connection= initialize.connect_to_vehicle('udpin:127.0.0.1:14550')
+
+max_speed = 55 / 3.6 # 55 km/h converted to m/s
+min_speed = 40 / 3.6 # 40 km/h converted to m/s
+
+vehicle_connection.mav.param_set_send(
+        vehicle_connection.target_system,
+        vehicle_connection.target_component,
+        b'AIRSPEED_MAX',
+        max_speed,
+        mavutil.mavlink.MAV_PARAM_TYPE_UINT32
+    )
+
+vehicle_connection.mav.param_set_send(
+        vehicle_connection.target_system,
+        vehicle_connection.target_component,
+        b'AIRSPEED_MIN',
+        min_speed,
+        mavutil.mavlink.MAV_PARAM_TYPE_UINT32
+    )
+
+def check_valid_speed(speed):
+    #PROMISES: Returns 0 if the speed value is not between the max and min values
+    #REQUIRES: "speed" is a positive, real number
+    
+    if(speed > max_speed):
+        print("ERROR: requested speed above maximum allowed speed")
+        return 0
+    if(speed < min_speed):
+        print("ERROR: requested speed below the minimum allowed speed")
+        return 0
+
 
 def change_speed(vehicle_connection, speed, throttle=-1):
     # PROMISES: The vehicle's speed can be dynamically changed
     # REQUIRES: Vehicle connection, and speed
+
+    if(check_valid_speed(speed) == 0): #if speed is invalid, exit function
+        return
 
     vehicle_connection.mav.command_long_send( # Specify COMMAND_LONG
         vehicle_connection.target_system, # Specify vehicle target system
@@ -30,6 +68,10 @@ def change_speed(vehicle_connection, speed, throttle=-1):
 def set_cruise_speed(vehicle_connection, speed):
     # PROMISES: Sets fixed-wing vehicle speed in m/s
     # REQUIRES: Vehicle connection, the desired vehicle speed
+
+    if(check_valid_speed(speed) == 0): #if speed is invalid, exit function
+        return
+
     vehicle_connection.mav.param_set_send(
         vehicle_connection.target_system,
         vehicle_connection.target_component,
