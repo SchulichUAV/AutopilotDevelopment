686b40ba754a9e994930b4ceb0ff263f8fd675c0
Oscar Spencer <oscarspencerdev@gmail.com>
2024-12-19 02:55:27 +0000

    new pr changes for liam

--- General/Operations/acknowledge.py
+++ General/Operations/acknowledge.py
@@ -1,6 +1,7 @@
-# mission_ack(47) implementation
-
 def mission_acknowledge(vehicle_connection):
+    # PROMISES: status of mission_ack (rec or not rec) 
+    # REQUIRES: vehicle connection
+    
     vehicle_connection.mav.mission_ack_send(
         vehicle_connection.target_system, 
         vehicle_connection.target_component,
@@ -10,6 +11,6 @@ def mission_acknowledge(vehicle_connection):
     msg = connection.recv_match(type='MISSION_ACK', blocking=True, timeout=5)
 
     if msg:
-        print("Recieved MISSION_ACK!")
+        print("Received MISSION_ACK!")
     else:
-        print("No MISSION_ACK response recieved!")
+        print("No MISSION_ACK response received!")
