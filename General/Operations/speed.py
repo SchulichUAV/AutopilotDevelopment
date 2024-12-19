02c82dcbb5694e446e6e86b0f4a883ab34b264ed
Liam Mah <liam.d.mah@gmail.com>
2024-12-18 22:32:11 -0700

    Adding explicit function parameter naming to speed and mode switching.

--- General/Operations/speed.py
+++ General/Operations/speed.py
@@ -11,17 +11,17 @@ def change_speed(vehicle_connection, speed, throttle=-1):
     # REQUIRES: Vehicle connection, and speed
 
     vehicle_connection.mav.command_long_send( # Specify COMMAND_LONG
-        vehicle_connection.target_system, # Specify vehicle target system
-        vehicle_connection.target_component, # Specify the vehicle target component
-        mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, # Specify speed command
-        0, # Confirmation - 0: First transmission of this cmd, 1-255: Confirmation transmissions (e.g. kill)
-        mavutil.mavlink.SPEED_TYPE_GROUNDSPEED, # Param 1 - setting speed type
-        speed, # Param 2 - speed in m/s
-        throttle, # Param 3 - throttle in percent
-        0, # Param 4 - Unused, set to zero to populate all 7 parameters
-        0, # Param 5 - Unused, set to zero to populate all 7 parameters
-        0, # Param 6 - Unused, set to zero to populate all 7 parameters
-        0 # Param 7 - Unused, set to zero to populate all 7 parameters
+        vehicle_system=vehicle_connection.target_system, # Specify vehicle target system
+        target_component=vehicle_connection.target_component, # Specify the vehicle target component
+        command=mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, # Specify speed command
+        confirmation=0, # Confirmation - 0: First transmission of this cmd, 1-255: Confirmation transmissions (e.g. kill)
+        speed_type=mavutil.mavlink.SPEED_TYPE_GROUNDSPEED, # Param 1 - setting speed type
+        speed=speed, # Param 2 - speed in m/s
+        throttle=throttle, # Param 3 - throttle in percent
+        _unused=0, # Param 4 - Unused, set to zero to populate all 7 parameters
+        _unused=0, # Param 5 - Unused, set to zero to populate all 7 parameters
+        _unused=0, # Param 6 - Unused, set to zero to populate all 7 parameters
+        _unused=0 # Param 7 - Unused, set to zero to populate all 7 parameters
     )
 
     msg = vehicle_connection.recv_match(type='COMMAND_ACK', blocking=True) # Print ACK to confirm successful execution
@@ -31,11 +31,11 @@ def set_cruise_speed(vehicle_connection, speed):
     # PROMISES: Sets fixed-wing vehicle speed in m/s
     # REQUIRES: Vehicle connection, the desired vehicle speed
     vehicle_connection.mav.param_set_send(
-        vehicle_connection.target_system,
-        vehicle_connection.target_component,
-        b'AIRSPEED_CRUISE',
-        speed,
-        mavutil.mavlink.MAV_PARAM_TYPE_UINT32
+        vehicle_system=vehicle_connection.target_system,
+        target_component=vehicle_connection.target_component,
+        param_id=b'AIRSPEED_CRUISE',
+        pitch_angle=speed,
+        param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT32
     )
 
     message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
