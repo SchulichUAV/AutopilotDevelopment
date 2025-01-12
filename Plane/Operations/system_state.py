861f77e8ccc03254e78f28bc599538d95c5c0436
user <steven03721@gmail.com>
2025-01-12 00:16:26 +0000

    Some basic error handling in system state file.

--- Plane/Operations/system_state.py
+++ Plane/Operations/system_state.py
@@ -5,26 +5,37 @@ from pymavlink import mavutil
 def receive_system_status(vehicle_connection):
     # PROMISES: Retreives general system state information of plane
     # REQUIRES: Vehicle connection
-    return vehicle_connection.recv_match(type='SYS_STATUS', blocking='True')
+    try: 
+        return vehicle_connection.recv_match(type='SYS_STATUS', blocking='True')
+    except Exception as e:
+        print(f"Error receiving system status: {e}")
+        return None
 
 def receive_param_request_read(vehicle_connection, param_id):
     # PROMISES: Retreives value of parameter thats passed in
     # REQUIRES: Vehicle connection, the desired parameter thats passed in
     param_id_bytes = param_id.encode('utf-8')
     
-    vehicle_connection.mav.param_request_read_send(
-        target_system=vehicle_connection.target_system,
-        target_component=vehicle_connection.target_component,
-        param_id=param_id_bytes,
-        param_index=-1, # -1 to use param ID field as identifier
-    )
+    try:
+        vehicle_connection.mav.param_request_read_send(
+            target_system=vehicle_connection.target_system,
+            target_component=vehicle_connection.target_component,
+            param_id=param_id_bytes,
+            param_index=-1, # -1 to use param ID field as identifier
+        )
+    except Exception as e:
+        print(f"Error sending param_request_read: {e}")
+        return None 
+    
 
     while True:
-        message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True, timeout=5)
-        if message is None:
-            print("Timeout: No response recieved.")
-            return None
-
+        try:
+            message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True, timeout=5)
+            if message is None:
+                print("Timeout: No response recieved.")
+                return None
+        except Exception as e:
+            print(f"Error receiving parameter value message: {e}")
         param_id_value = message.param_id
 
         if isinstance(param_id_value, bytes):
@@ -38,7 +49,15 @@ def receive_param_request_read(vehicle_connection, param_id):
 def receive_gps_raw(vehicle_connection):
     # PROMISES: Retrieves general gps data
     # REQUIRES: Vehicle connection
-    return vehicle_connection.recv_match(type='GPS_RAW_INT', blocking='True')
+    try:
+        message = vehicle_connection.recv_match(type='GPS_RAW_INT', blocking='True')
+        if message is None:
+            print("Timeout: No GPS data received.")
+            return None
+        return message
+    except Exception as e:
+        print(f"Error receiving GPS data: {e}")
+        return None
 
 # def receive_gps_status(vehicle_connection):
 #     # PROMISES:
