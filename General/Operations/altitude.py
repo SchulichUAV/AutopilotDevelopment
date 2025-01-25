68c59e3a55e22545e3bb0e9648b1b0deee3e937f
Liam Mah <liam.d.mah@gmail.com>
2025-01-24 20:26:47 -0700

    Redoing altitude command.

--- General/Operations/altitude.py
+++ General/Operations/altitude.py
@@ -2,16 +2,23 @@ from pymavlink import mavutil
 
 
 def set_current_altitude(vehicle_connection, altitude):
-
-    msg = vehicle_connection.mav.command_long_send(
-            vehicle_connection.target_system,
-            vehicle_connection.target_component,
-            mavutil.mavlink.MAV_CMD_NAV_ALTITUDE,
-            0,
-            altitude,
-            0, 0, 0, 0, 0, 0
+    # PROMISES: The vehicle will fly to the spcified height
+    # REQUIRES: Vehicle connection, desired altitude (AGL in metres)
+    vehicle_connection.mav.command_int_send(
+        target_system=vehicle_connection.target_system,
+        target_component=vehicle_connection.target_component,
+        frame=mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, # Global (WGS84) coordinate frame + altitude relative to home position
+        command=mavutil.mavlink.MAV_CMD_DO_CHANGE_ALTITUDE,
+        current=0, # Not used here, arbitrary value
+        autocontinue=0, # Not used here, arbitrary value
+        param1=altitude
+        param2=mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, 
+        param3=0, # Not used here, arbitrary value
+        param4=0, # Not used here, arbitrary value
+        x=0, # Not used here, arbitrary value
+        y=0, # Not used here, arbitrary value
+        z=0 # Not used here, arbitrary value
     )
 
-    vehicle_connection.mav.send(msg)
-
-    print('Sent new altitude value set to {altitude} meters.')
+    msg = vehicle_connection.recv_match(type='COMMAND_ACK', blocking=True)
+    print(msg)
