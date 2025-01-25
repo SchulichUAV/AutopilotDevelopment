de105746fe677b76e3332117e65ce64f31ead808
Liam Mah <liam.d.mah@gmail.com>
2025-01-24 21:14:37 -0700

    Revising some altitude stuff. Verified to work in sim.

--- Plane/TestScripts/sitl_change_altitude.py
+++ Plane/TestScripts/sitl_change_altitude.py
@@ -10,4 +10,4 @@ import Plane.Operations.altitude as altitude
 vehicle_connection, valid_connection = initialize.connect_to_vehicle('udpin:127.0.0.1:14550')
 
 mode.set_mode(vehicle_connection, 15)
-altitude.set_current_altitude(100)
+altitude.set_current_altitude(vehicle_connection, 90)
