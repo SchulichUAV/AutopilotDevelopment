0ab94ea806ed06b7632c0c93147ead833525b8ef
Nathan <nathanjourdain26@gmail.com>
2025-02-01 22:10:32 +0000

    Added TryCatch functionality to files in the Plane/Operations folder

--- Plane/Operations/takeoff.py
+++ Plane/Operations/takeoff.py
@@ -7,30 +7,36 @@ from pymavlink import mavutil
 def set_takeoff_altitude(vehicle_connection, takeoff_height):
     # PROMISES: Sets fixed-wing vehicle takeoff altitude configuration
     # REQUIRES: Vehicle connection, the desired height to take off to
-    vehicle_connection.mav.param_set_send(
-        target_system=vehicle_connection.target_system,
-        target_component=vehicle_connection.target_component,
-        param_id=b'TKOFF_ALT', # takeoff altitude in metres
-        param_value=takeoff_height,
-        param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT32
-    )
+    try:
+        vehicle_connection.mav.param_set_send(
+            target_system=vehicle_connection.target_system,
+            target_component=vehicle_connection.target_component,
+            param_id=b'TKOFF_ALT', # takeoff altitude in metres
+            param_value=takeoff_height,
+            param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT32
+        )
 
-    message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
-    print('name: %s\tvalue: %d' %
-        (message['param_id'], message['param_value']))
+        message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
+        print('name: %s\tvalue: %d' %
+            (message['param_id'], message['param_value']))
+    except Exception as e:
+        print(f"Error in function: set_takeoff_altitude() from file: Plane/Operations/takeoff.py -> {e}")
 
 def set_takeoff_angle(vehicle_connection, takeoff_pitch_angle):
     # PROMISES: Sets fixed-wing vehicle takeoff pitch angle configuration
     # REQUIRES: Vehicle connection, the pitch ascent angle
-    vehicle_connection.mav.param_set_send(
-        target_system=vehicle_connection.target_system,
-        target_component=vehicle_connection.target_component,
-        param_id=b'TKOFF_LVL_PITCH',
-        param_value=takeoff_pitch_angle,
-        param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT32
-    )
+    try:
+        vehicle_connection.mav.param_set_send(
+            target_system=vehicle_connection.target_system,
+            target_component=vehicle_connection.target_component,
+            param_id=b'TKOFF_LVL_PITCH',
+            param_value=takeoff_pitch_angle,
+            param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT32
+        )
 
-    message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
-    print('name: %s\tvalue: %d' %
-        (message['param_id'], message['param_value']))
-    
+        message = vehicle_connection.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
+        print('name: %s\tvalue: %d' %
+            (message['param_id'], message['param_value']))
+    except Exception as e:
+        print(f"Error in function: set_takeoff_angle() from file: Plane/Operations/takeoff.py -> {e}")
+
