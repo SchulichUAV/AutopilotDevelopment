7510b1806b99ca9a2ea692a258b770c1cf5d63a2
Liam Mah <liam.d.mah@gmail.com>
2025-03-02 11:00:44 -0700

    Fixing vehicle connection.

--- General/Operations/initialize.py
+++ General/Operations/initialize.py
@@ -15,6 +15,7 @@ def verify_connection(vehicle_connection):
         return True
     except Exception as e:
         print(f"Error in function: verify_connection() from file: General/Operations/initialize.py -> {e}")
+        return False
 
 def connect_to_vehicle(port='172.23.192.1:14500'):
     # PROMISES: Connection to the vehicle will be established
@@ -22,7 +23,11 @@ def connect_to_vehicle(port='172.23.192.1:14500'):
     # Connect to the vehicle
     try:
         vehicle_connection = mavutil.mavlink_connection(port)
-        valid_connection = verify_connection(vehicle_connection)   
-        return vehicle_connection, valid_connection
+        valid_connection = verify_connection(vehicle_connection)
+        if valid_connection:
+            return vehicle_connection
+        else:
+            print("Error trying to verify connection. Exiting.")
+            exit(1)
     except Exception as e:
         print(f"Error in function: connect_to_vehicle() from file: General/Operations/initialize.py -> {e}")
