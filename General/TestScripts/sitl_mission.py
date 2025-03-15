11da19d93332a4cefc225b68282676eb177c5d13
Thomas Mattern <thomasmattern1@gmail.com>
2025-03-15 12:25:13 -0600

    Minor naming improvement

--- General/TestScripts/sitl_mission.py
+++ General/TestScripts/sitl_mission.py
@@ -13,10 +13,10 @@ vehicle_connection = initialize.connect_to_vehicle('udpin:172.20.128.1:14550')
 
 waypoints = {}
 # dummy waypoint
-waypoints[0] = waypoint.set_mission_waypoint(vehicle_connection, -35.3603037, 149.1628575, 50, 0)
-waypoints[1] = waypoint.set_mission_waypoint(vehicle_connection, -35.3603037, 149.1628575, 50, 1)
-waypoints[2] = waypoint.set_mission_waypoint(vehicle_connection, -35.3609686, 149.1606474, 140, 2)
-waypoints[3] = waypoint.set_mission_loiter_waypoint(vehicle_connection, -35.3620974, 149.1624109, 190, 5, 3)
+waypoints[0] = waypoint.create_mission_nav_waypoint(vehicle_connection, -35.3603037, 149.1628575, 50, 0)
+waypoints[1] = waypoint.create_mission_nav_waypoint(vehicle_connection, -35.3603037, 149.1628575, 50, 1)
+waypoints[2] = waypoint.create_mission_nav_waypoint(vehicle_connection, -35.3609686, 149.1606474, 140, 2)
+waypoints[3] = waypoint.create_mission_loiter_waypoint(vehicle_connection, -35.3620974, 149.1624109, 190, 5, 3)
 
 mission.upload_mission_waypoints(vehicle_connection, waypoints)
 mode.set_mode(vehicle_connection, 10)
--- Plane/Operations/waypoint.py
+++ Plane/Operations/waypoint.py
@@ -17,10 +17,10 @@ def set_waypoint(vehicle_connection, latitude, longitude, altitude, autocontinue
             command=dialect.MAV_CMD_NAV_WAYPOINT,
             current=2, # Current can be set to 2 to work in guided mode
             autocontinue=autocontinue, # Autocontinue to next waypoint (0 to pause mission after item completes)
-            param1=0, # Hold time [s] at waypoint, ignored by fixed wing 
+            param1=0, # Ignored
             param2=0, # Acceptance radius [m] (if sphere with this radius is hit, waypoint counts as reached) 
             param3=0, # Pass radius, 0 to pass through WP. if >0, will CW orbit, <0 will have CCW orbit [m]
-            param4=0, # Desired yaw [degrees] at WP (NaN to use current system yaw heading mode)
+            param4=0, # Ignored
             x=int(latitude * 1e7), # latitude in degrees *10^7
             y=int(longitude * 1e7), # longitude in degrees *10^7
             z=altitude # altitude [m] - relative or absolute depending on frame
@@ -32,7 +32,7 @@ def set_waypoint(vehicle_connection, latitude, longitude, altitude, autocontinue
     except Exception as e:
         print(f"Error in function: set_waypoint() from file: Plane/Operations/waypoint.py -> {e}")
 
-def set_mission_waypoint(vehicle_connection, latitude, longitude, altitude, seq):
+def create_mission_nav_waypoint(vehicle_connection, latitude, longitude, altitude, seq):
     # PROMISES: Will return a MISSION_ITEM_INT message
     # REQUIRES: a vehicle connection, latitude, longitude, altitude, an autocontinue flag, and a sequence number for the waypoint.
     try:
@@ -42,7 +42,7 @@ def set_mission_waypoint(vehicle_connection, latitude, longitude, altitude, seq)
             seq=seq, # Waypoint ID
             frame=dialect.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # Coordinate system of waypoint
             command=dialect.MAV_CMD_NAV_WAYPOINT,
-            current=0, # Current should be set to 0 for missions
+            current=0, # Current should be set to 0 for missions, really only used for download
             autocontinue=0, # Ignored by ArduPlane, as fixed-wing aircraft continuously follow mission waypoints in and do not pause at waypoints.
             param1=0, # Ignored 
             param2=0, # Acceptance radius [m] (if sphere with this radius is hit, waypoint counts as reached) 
@@ -57,7 +57,7 @@ def set_mission_waypoint(vehicle_connection, latitude, longitude, altitude, seq)
     except Exception as e:
         print(f"Error in function: set_mission_waypoint() from file: Plane/Operations/waypoint.py -> {e}")
 
-def set_mission_loiter_waypoint(vehicle_connection, latitude, longitude, altitude, radius, seq):
+def create_mission_loiter_waypoint(vehicle_connection, latitude, longitude, altitude, radius, seq):
     # PROMISES: Will return a MISSION_ITEM_INT message
     # REQUIRES: a vehicle connection, latitude, longitude, altitude, waypoint radius, and a sequence number for the waypoint.
     try:
