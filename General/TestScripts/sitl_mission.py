a7fef4cdfb6e593adf4d96d23cf0a188c039364e
Thomas Mattern <thomasmattern1@gmail.com>
2026-05-29 10:28:07 -0600

    change up mission to drop on waypoint instead

--- General/TestScripts/sitl_mission.py
+++ General/TestScripts/sitl_mission.py
@@ -1,15 +1,12 @@
 import sys
 import os
 
-# 1. Anchor to this file's folder, then jump up 4 steps to hit the '2025Electronics' root
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, "..", "..", "..", ".."))
 
-# 2. Add the root directory to the Python path
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-# 3. Now the modules imports will resolve perfectly!
 import modules.AutopilotDevelopment.General.Operations.initialize as initialize
 import modules.AutopilotDevelopment.General.Operations.mode as mode
 import modules.AutopilotDevelopment.General.Operations.mission as mission
@@ -17,8 +14,6 @@ import modules.AutopilotDevelopment.Plane.Operations.waypoint as waypoint
 
 # 3. Rest of your connection code...
 vehicle_connection = initialize.connect_to_vehicle('udpin:0.0.0.0:14550')
-
 # object_location = {"lat":-35.36500000, "lon": 149.16700000, "alt": 60.0}
 
-mission.upload_payload_drop_mission(vehicle_connection)
-mission.check_distance_and_drop(vehicle_connection, 0, None, None)
+mission.start_copter_drop_mission(vehicle_connection, None)
